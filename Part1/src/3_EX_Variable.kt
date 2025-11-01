2ef4a0fe552f19a5983113ba454f1239c596528a
Chanhee Park <chanheep829@gmail.com>
2025-11-01 10:08:30 -0700

    commit of Part1 files

---
+++ Part1/src/3_EX_Variable.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>): Unit
+{
+    var total: Int // 변수 선언문 (declare variable)
+    total = 0
+
+    val a: Int = 10 + 53 - 7
+    println(a)
+
+    val b: Int = 43 + 75 + a
+    println(b)
+
+    total = a + b
+    println(total)
+}
